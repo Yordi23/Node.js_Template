cd9495380507338407a0d28a8d90e32e3b6912ed
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-04-18 20:30:21 -0400

    Added mddleewares and other changes

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
 const NARouter = require("./routes/NARoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
 const app = express();
 
-//Middlewares
+//###Middlewares###
+
+//Development loggin
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+//Body parser
 app.use(express.json());
 
 //Routes
 app.use("/api/v1/NA", NARouter);
+
+//###Error Handling###
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
